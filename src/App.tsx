cbf6f175e0d918ebac2f115d5e7260198c2d6d85
limbs722 <limbs722@gmail.com>
2022-02-17 21:04:50 +0900

    Add - UI 라이브러리, 검색 UI 추가
    Antd 라이브러리 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { AutoComplete } from 'antd';
+import 'antd/dist/antd.min.css';
+import 'assets/css/AntOverride.scss';
+import 'assets/css/App.scss';
+
+interface KeywordProps {
+  type: string;
+  name: string;
+}
 
 export default function App() {
-  return <div>App</div>;
+  const [isFetching, setIsFetching] = useState(false);
+  const [keywordList, setKeywordList] = useState<Array<KeywordProps>>([]);
+
+  function onChange() {}
+
+  function onSearch() {}
+
+  const formSearchAttr = {
+    placeholder: 'Keyword',
+    onChange: onChange,
+    onSearch: onSearch,
+  };
+
+  // 검색결과
+
+  return (
+    <div className="wrapper-main">
+      <div className="search-wrapper">
+        <AutoComplete {...formSearchAttr} />
+      </div>
+      <div>{/** 데이터 리스트 */}</div>
+    </div>
+  );
 }
